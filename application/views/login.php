b66e4f1a0974c835ff1b805e5be06285b781a301
Ishan Nadeera <shanf753@gmail.com>
2022-07-13 20:42:50 +0530

    Created forms to login and register

--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,20 @@
 <?php include 'Partials/header.php'?>
 
-<h2>Login</h2>
+<div class="container">
+    <h2>Login</h2>
+
+    <hr>
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+</div>
 
 <?php include 'Partials/footer.php'?>
--- application/views/register.php
+++ application/views/register.php
@@ -1,5 +1,28 @@
 <?php include 'Partials/header.php'?>
 
-<h2>Register</h2>
+<div class="container">
+    <h2>Register</h2>
+
+    <hr>
+    <form>
+        <div class="form-group">
+            <label for="exampleInputFirstName1">First name</label>
+            <input type="text" class="form-control" id="exampleInputFirstName1" placeholder="First name">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputLastName1">Last name</label>
+            <input type="text" class="form-control" id="exampleInputLastName1" placeholder="Last name">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+</div>
 
 <?php include 'Partials/footer.php'?>
